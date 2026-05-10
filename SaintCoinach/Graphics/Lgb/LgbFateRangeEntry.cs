d09cc7237b3da202e7b62c10d6514e9f16429701
ClayLivince <41274562+ClayLivince@users.noreply.github.com>
2026-05-10 16:47:41 +0800

    core: backport non-multipack engine improvements from cn master
    - LgbFateRangeEntry: real implementation with translation/rotation/scale
    - SheetUpdater: try/catch around row matching to continue on error
    - Quest.ExpansionCode helper

---
+++ SaintCoinach/Graphics/Lgb/LgbFateRangeEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintCoinach.Graphics.Lgb {
+    public class LgbFateRangeEntry : ILgbEntry {
+        #region Struct
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HeaderData {
+            public LgbEntryType Type;
+            public uint UnknownId;             // This ID is Level
+            public int NameOffset;
+            public Vector3 Translation;
+            public Vector3 Rotation;
+            public Vector3 Scale;
+            // 24 bytes of unknowns
+        }
+        #endregion
+
+        #region Properties
+        public LgbEntryType Type => Header.Type;
+        public HeaderData Header { get; private set; }
+        public string Name { get; private set; }
+        #endregion
+
+        #region Constructor
+        public LgbFateRangeEntry(IO.PackCollection packs, byte[] buffer, int offset) {
+            this.Header = buffer.ToStructure<HeaderData>(offset);
+        }
+        #endregion
+
+    }
+}
